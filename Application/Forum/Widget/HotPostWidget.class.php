64a483125d293302728940a6945a5afb65cda67f
陈一枭 <yhtt2020@foxmail.com>
2014-03-12 18:00:43 +0800

    新增热门

--- Application/Forum/Widget/HotPostWidget.class.php
+++ Application/Forum/Widget/HotPostWidget.class.php
@@ -7,7 +7,7 @@
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-namespace Home\Widget;
+namespace Forum\Widget;
 use Think\Action;
 
 /**
