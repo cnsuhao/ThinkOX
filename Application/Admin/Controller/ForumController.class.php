5bb4dd04348936e538986206c7828f3ae9a070e8
caipeichao <346089341@qq.com>
2014-03-13 16:32:15 +0800

    新增 后台帖子编辑功能

--- Application/Admin/Controller/ForumController.class.php
+++ Application/Admin/Controller/ForumController.class.php
@@ -122,13 +122,58 @@ class ForumController extends AdminController {
         $builder = new AdminListBuilder();
         $builder->title('帖子管理' . $forumTitle)
             ->buttonNew(U('editPost'))
-            ->keyId()->keyTitle()->keyCreateTime()->keyUpdateTime()->keyTime('last_reply_time','最后回复时间')->keyStatus()->keyHtml('DOACTIONS', '操作')
+            ->keyId()->keyTitle()->keyCreateTime()->keyUpdateTime()->keyTime('last_reply_time','最后回复时间')->keyHtml('DOACTIONS', '操作')
             ->data($list)
             ->pagination($totalCount, 20)
             ->display();
     }
 
     public function editPost($id=null) {
+        //判断是否在编辑模式
+        $isEdit = $id ? true : false;
 
+        //读取帖子内容
+        if($isEdit) {
+            $post = M('ForumPost')->where(array('id'=>$id))->find();
+        } else {
+            $post = array();
+        }
+
+        //读取贴吧列表
+        $forumList = M('Forum')->where(array('status'=>1))->order('id asc')->select();
+        $forums = array();
+        foreach($forumList as &$e) {
+            $forums[$e['id']] = $e['title'];
+        }
+
+        //显示页面
+        $builder = new AdminConfigBuilder();
+        $builder->title($isEdit ? '编辑帖子' : '新建帖子')
+            ->keyId()->keyTitle()->keyEditor('content','内容')->keyCreateTime()->keyUpdateTime()->keyTime('last_reply_time','最后回复时间')->keySelect('forum_id','所属贴吧',null,$forums)
+            ->buttonSubmit(U('doEditPost'))->buttonBack()
+            ->data($post)
+            ->display();
+    }
+
+    public function doEditPost($id=null,$title,$content,$create_time,$update_time,$last_reply_time) {
+        //判断是否为编辑模式
+        $isEdit = $id ? true : false;
+
+        //写入数据库
+        $model = M('ForumPost');
+        $data = array('title'=>$title,'content'=>$content,'create_time'=>$create_time,'update_time'=>$update_time,'last_reply_time'=>$last_reply_time);
+        if($isEdit) {
+            $result = $model->where(array('id'=>$id))->save($data);
+        } else {
+            $result = $model->add($data);
+        }
+
+        //如果写入不成功，则报错
+        if(!$result) {
+            $this->error($isEdit ? '编辑失败' : '创建成功');
+        }
+
+        //返回成功信息
+        $this->success($isEdit ? '编辑成功' : '创建成功');
     }
 }
