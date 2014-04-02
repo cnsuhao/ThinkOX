aca2eb18f77a648c418c5a2ab10fbbc3f9165b5a
caipeichao <346089341@qq.com>
2014-04-02 09:43:11 +0800

    修复 Api无法调用的问题

---
+++ Application/Common/Api/Api.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caipeichao
+ * Date: 4/2/14
+ * Time: 9:14 AM
+ */
+
+namespace Common\Api;
+
+class Api {
+    public function apiSuccess() {
+
+    }
+
+    public function apiError() {
+
+    }
+}
---
+++ Application/Weibo/Api/WeiboApi.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caipeichao
+ * Date: 4/2/14
+ * Time: 9:14 AM
+ */
+
+use Common\Api\Api;
+
