1e99216825503751d6842d5b74be8589f34daa73
caipeichao <346089341@qq.com>
2014-04-11 15:10:49 +0800

    格式化代码

--- Application/Common/Common/pagination.php
+++ Application/Common/Common/pagination.php
@@ -6,43 +6,53 @@
  * Time: PM7:40
  */
 
-function getPagination($totalCount, $countPerPage=10) {
+function getPagination($totalCount, $countPerPage = 10)
+{
     $pageKey = 'page';
+
     //获取当前页码
     $currentPage = $_REQUEST[$pageKey] ? $_REQUEST[$pageKey] : 1;
+
     //计算总页数
     $pageCount = ceil($totalCount / $countPerPage);
+
     //如果只有1页，就没必要翻页了
-    if($pageCount <= 1) {
+    if ($pageCount <= 1) {
         return '';
     }
+
     //定义返回结果
     $html = '';
+
     //添加头部
     $html .= '<div class="pagination">';
+
     //添加上一页的按钮
-    if($currentPage>1) {
-        $prevUrl = addUrlParam(getCurrentUrl(), array($pageKey=>$currentPage-1));
+    if ($currentPage > 1) {
+        $prevUrl = addUrlParam(getCurrentUrl(), array($pageKey => $currentPage - 1));
         $html .= "<li><a class=\"\" href=\"{$prevUrl}\">&laquo;</a></li>";
     } else {
         $html .= "<li class=\"disabled\"><a>&laquo;</a></li>";
     }
+
     //添加各页面按钮
-    for($i=1;$i<=$pageCount;$i++){
-        $pageUrl = addUrlParam(getCurrentUrl(), array($pageKey=>$i));
-        if($i==$currentPage) {
+    for ($i = 1; $i <= $pageCount; $i++) {
+        $pageUrl = addUrlParam(getCurrentUrl(), array($pageKey => $i));
+        if ($i == $currentPage) {
             $html .= "<li class=\"active\"><a class=\"active\" href=\"{$pageUrl}\">{$i}</a></li>";
         } else {
             $html .= "<li><a class=\"\" href=\"{$pageUrl}\">{$i}</a></li>";
         }
     }
+
     //添加下一页按钮
-    if($currentPage < $pageCount) {
-        $nextUrl = addUrlParam(getCurrentUrl(), array($pageKey=>$currentPage+1));
+    if ($currentPage < $pageCount) {
+        $nextUrl = addUrlParam(getCurrentUrl(), array($pageKey => $currentPage + 1));
         $html .= "<li><a class=\"\" href=\"{$nextUrl}\">&raquo;</a></li>";
     } else {
         $html .= "<li class=\"disabled\"><a>&raquo;</a></li>";
     }
+
     //收尾
     $html .= '</div>';
     return $html;
@@ -51,18 +61,16 @@ function getPagination($totalCount, $countPerPage=10) {
 
 function getPageHtml($f_name, $totalpage, $data, $nowpage)
 {
-
-    if ($totalpage  > 1 && $totalpage != null) {
+    if ($totalpage > 1 && $totalpage != null) {
         $str = '';
         foreach ($data as $k => $v) {
             $str = $str . '"' . $v . '"' . ',';
         }
         $pages = '';
         for ($i = 1; $i <= $totalpage; $i++) {
-            if($i == $nowpage){
+            if ($i == $nowpage) {
                 $pages = $pages . "<li class=\"active\"><a href=\"javascript:\" id='page_" . $i . "' class='page active' onclick='" . $f_name . "(" . $str . $i . ")'>" . $i . "</a></li>";
-            }
-            else{
+            } else {
                 $pages = $pages . "<li><a href=\"javascript:\" id='page_" . $i . "' class='page' onclick='" . $f_name . "(" . $str . $i . ")'>" . $i . "</a></li>";
             }
         }
@@ -73,7 +81,7 @@ function getPageHtml($f_name, $totalpage, $data, $nowpage)
             $a = $nowpage - 1;
             $pre = "<li><a href=\"javascript:\" class='page_pre'  onclick = '" . $f_name . "( " . $str . $a . ")'>" . "&laquo;" . "</a></li>";
         }
-    /*    $pre = "<li class=\"disabled\"><a class='a page_pre'  onclick = '" . $f_name . "( " . $str . $a . ")'>" . "上一页" . "</a></li>";*/
+        /*    $pre = "<li class=\"disabled\"><a class='a page_pre'  onclick = '" . $f_name . "( " . $str . $a . ")'>" . "上一页" . "</a></li>";*/
 
         if ($nowpage == $totalpage) {
             $b = $totalpage;
@@ -88,13 +96,15 @@ function getPageHtml($f_name, $totalpage, $data, $nowpage)
 }
 
 
-function getPage($data,$limit,$page){
-    $offset = ($page-1)*$limit;
-    return array_slice($data,$offset,$limit);
+function getPage($data, $limit, $page)
+{
+    $offset = ($page - 1) * $limit;
+    return array_slice($data, $offset, $limit);
 }
 
 
-function addUrlParam($url, $params) {
+function addUrlParam($url, $params)
+{
     $app = MODULE_NAME;
     $controller = CONTROLLER_NAME;
     $action = ACTION_NAME;
@@ -102,6 +112,7 @@ function addUrlParam($url, $params) {
     return U("$app/$controller/$action", $get);
 }
 
-function getCurrentUrl() {
+function getCurrentUrl()
+{
     return $_SERVER['REQUEST_URI'];
 }
