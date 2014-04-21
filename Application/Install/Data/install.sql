107f290c6274519b0e78e6df058ad5b433b5540c
caipeichao <346089341@qq.com>
2014-04-21 14:45:38 +0800

    修改头衔的初始ID

--- Application/Install/Data/install.sql
+++ Application/Install/Data/install.sql
@@ -1492,7 +1492,7 @@ CREATE TABLE IF NOT EXISTS `thinkox_rank` (
   `logo` int(11) NOT NULL,
   `create_time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=10000 ;
 
 --
 -- 表的结构 `thinkox_rank_user`
@@ -1505,7 +1505,7 @@ CREATE TABLE IF NOT EXISTS `thinkox_rank_user` (
   `is_show` tinyint(4) NOT NULL COMMENT '是否显示在昵称右侧（必须有图片才可）',
   `create_time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=10000 ;
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
