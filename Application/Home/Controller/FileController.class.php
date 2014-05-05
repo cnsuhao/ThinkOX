01e99ed292f7bc179f8db36909059253505a4343
陈一枭 <yhtt2020@foxmail.com>
2014-05-05 16:10:14 +0800

    专辑

--- Application/Home/Controller/FileController.class.php
+++ Application/Home/Controller/FileController.class.php
@@ -52,4 +52,40 @@ class FileController extends HomeController {
 		}
 		
 	}
+
+
+
+    /**
+     * 上传图片
+     * @author huajie <banhuajie@163.com>
+     */
+    public function uploadPicture(){
+        //TODO: 用户登录检测
+
+        /* 返回标准数据 */
+        $return  = array('status' => 1, 'info' => '上传成功', 'data' => '');
+
+        /* 调用文件上传组件上传文件 */
+        $Picture = D('Admin/Picture');
+        $pic_driver = C('PICTURE_UPLOAD_DRIVER');
+        $info = $Picture->upload(
+            $_FILES,
+            C('PICTURE_UPLOAD'),
+            C('PICTURE_UPLOAD_DRIVER'),
+            C("UPLOAD_{$pic_driver}_CONFIG")
+        ); //TODO:上传到远程服务器
+
+        /* 记录图片信息 */
+        if($info){
+            $return['status'] = 1;
+            $return = array_merge($info['download'], $return);
+        } else {
+            $return['status'] = 0;
+            $return['info']   = $Picture->getError();
+        }
+
+        /* 返回JSON数据 */
+        $this->ajaxReturn($return);
+    }
+
 }
--- Application/Issue/Controller/IndexController.class.php
+++ Application/Issue/Controller/IndexController.class.php
@@ -14,14 +14,66 @@ class IndexController extends Controller
      */
     public function _initialize()
     {
-        $tree=D('Issue')->getTree();
-        $this->assign('tree',$tree);
+        $tree = D('Issue')->getTree();
+        $this->assign('tree', $tree);
     }
 
-    public function index()
+    public function index($page=1,$issue_id=0)
     {
+        $issue=D('Issue')->find($issue_id);
+        if(!$issue_id==0){
+            $issue_id=intval($issue_id);
+            $issues=D('Issue')->where('id='.$issue_id.' OR pid='.$issue_id)->limit(999)->select();
+            $ids=array();
+            foreach($issues as $v)
+            {
+                $ids[]=$v['id'];
+            }
+            $map['issue_id']=array('in',implode(',',$ids));
+        }
+        $map['status']=1;
+        $content=D('IssueContent')->where($map)->order('create_time desc')->page($page,16)->select();
+        $totalCount=D('IssueContent')->where($map)->count();
+        foreach($content as &$v){
+            $v['user']=query_user(array('id','username','space_url','space_link','avatar128','rank_html'),$v['uid']);
+        }
+        unset($v);
+        $this->assign('contents',$content);
+        $this->assign('totalPageCount',$totalCount);
+        $this->assign('top_issue',$issue['pid']==0?$issue['id']:$issue['pid']);
+
         $this->display();
     }
 
+    public function doPost($cover_id = 0, $title = '', $content = '', $issue_id = 0)
+    {
+        if (!$cover_id) {
+            $this->error('请上传封面。');
+        }
+        if (trim(op_t($title)) == '') {
+            $this->error('请输入标题。');
+        }
+        if (trim(op_h($content)) == '') {
+            $this->error('请输入内容。');
+        }
+        if ($issue_id == 0) {
+            $this->error('请选择分类。');
+        }
+        $content = D('IssueContent')->create();
+        $rs = D('IssueContent')->add($content);
+        if ($rs) {
+            $this->success('投稿成功。', 'refresh');
+        } else {
+            $this->success('投稿失败。', '');
+        }
 
+    }
+
+    public function selectDropdown($pid)
+    {
+        $issues = D('Issue')->where(array('pid' => $pid, 'status' => 1))->limit(999)->select();
+        exit(json_encode($issues));
+
+
+    }
 }
