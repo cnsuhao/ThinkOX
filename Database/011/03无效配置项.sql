6ad301f0894697a6b941e4fb0169703538d414f8
陈一枭 <yhtt2020@foxmail.com>
2014-05-11 16:13:32 +0800

    数据库

---
+++ Database/011/03无效配置项.sql
@@ -0,0 +1,3 @@
+DELETE FROM `thinkox_thinkox_config` WHERE `thinkox_config`.`id` = 1;
+DELETE FROM `thinkox_thinkox_config` WHERE `thinkox_config`.`id` = 2;
+DELETE FROM `thinkox_thinkox_config` WHERE `thinkox_config`.`id` = 3;
--- Database/011/zan.sql
+++
@@ -1,3 +0,0 @@
-Insert INTO `{$db_prefix}hooks` ( `name`, `description`, `type`, `update_time`, `addons`) VALUES
-( 'support', '赞', 1, 1398264759, 'Support');
-
