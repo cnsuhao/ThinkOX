f9a6d47d5754792dac713d471f35ff32ee62c7b4
陈一枭 <yhtt2020@foxmail.com>
2014-05-27 23:55:50 +0800

    在兼容老page的基础上加入TS形式的findPage方法快速分页。

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -21,7 +21,7 @@ class Page{
 
     private $p       = 'page'; //分页参数名
     private $url     = ''; //当前链接URL
-    private $nowPage = 1;
+    public  $nowPage = 1;
 
 	// 分页显示定制
     private $config  = array(
