32b06b3f1abf0fc29ea69b215264776bcbea9e28
陈一枭 <yhtt2020@foxmail.com>
2014-06-05 13:56:28 +0800

    帖子详情安全过滤

--- Public/static/ueditormini/js/umeditor.config.js
+++ Public/static/ueditormini/js/umeditor.config.js
@@ -161,7 +161,7 @@
         //,langPath:URL +"lang/"
 
         //ie下的链接自动监测
-        //,autourldetectinie:false
+        ,autourldetectinie:true
 
         //主题配置项,默认是default。有需要的话也可以使用如下这样的方式来自动多主题切换，当然，前提条件是themes文件夹下存在对应的主题文件：
         //现有如下皮肤:default
