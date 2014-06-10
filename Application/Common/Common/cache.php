14c101e52919f71c1722c922a77342d119438485
陈一枭 <yhtt2020@foxmail.com>
2014-06-10 15:05:51 +0800

    加入清理全站缓存方法 clean_all_cache

--- Application/Common/Common/cache.php
+++ Application/Common/Common/cache.php
@@ -13,11 +13,49 @@
  * @param $func
  * @return mixed
  */
-function op_cache($key, $func, $interval) {
+function op_cache($key, $func, $interval)
+{
     $result = S($key);
-    if(!$result) {
+    if (!$result) {
         $result = $func();
         S($key, $result, $interval);
     }
     return $result;
 }
+
+function clean_all_cache()
+{
+    $dirname = './Runtime/';
+
+//清文件缓存
+    $dirs = array($dirname);
+//清理缓存
+    foreach ($dirs as $value) {
+        rmdirr($value);
+    }
+    @mkdir($dirname, 0777, true);
+}
+
+
+function rmdirr($dirname)
+{
+    if (!file_exists($dirname)) {
+        return false;
+    }
+    if (is_file($dirname) || is_link($dirname)) {
+        return unlink($dirname);
+    }
+    $dir = dir($dirname);
+    if ($dir) {
+        while (false !== $entry = $dir->read()) {
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+            rmdirr($dirname . DIRECTORY_SEPARATOR . $entry);
+        }
+    }
+    $dir->close();
+    return rmdir($dirname);
+}
+
+
