12cc3213d492beefb3c65f34a8a9e587f9191713
陈一枭 <yhtt2020@foxmail.com>
2014-06-12 21:43:17 +0800

    自动升级临时提交

---
+++ Application/Admin/Controller/UpdateController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caipeichao
+ * Date: 14-3-14
+ * Time: AM10:59
+ */
+
+namespace Admin\Controller;
+class UpdateController extends AdminController
+{
+    public function update($page = 1, $r = 20)
+    {
+        echo'升级成功。';
+    }
+
+
+}
