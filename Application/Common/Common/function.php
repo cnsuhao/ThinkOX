765790ac0fbe6143c13adb110aee43733f4097d7
xiaojuntao <xiaojuntao>
2014-06-16 10:40:24 +0800

    class 过滤

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1184,6 +1184,38 @@ function is_ie()
     }
 }
 
+
+
+function replace_style($content){
+    $content =  preg_replace("/class=\".*?\"/si","",$content);
+    $content = closetags($content);
+    return $content;
+
+}
+
+function closetags($html) {
+    preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
+    $openedtags = $result[1];
+
+    preg_match_all('#</([a-z]+)>#iU', $html, $result);
+    $closedtags = $result[1];
+    $len_opened = count($openedtags);
+
+    if (count($closedtags) == $len_opened) {
+        return $html;
+    }
+    $openedtags = array_reverse($openedtags);
+
+    for ($i=0; $i < $len_opened; $i++) {
+        if (!in_array($openedtags[$i], $closedtags)){
+            $html .= '</'.$openedtags[$i].'>';
+        } else {
+            unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+        }
+    }
+    return $html;
+}
+
 function array_subtract($a, $b)
 {
     return array_diff($a, array_intersect($a, $b));
