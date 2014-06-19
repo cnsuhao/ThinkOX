2e05192f70e86fb1ac35adc5d564e5b3360eb432
郑钟良 <WebServiceSoftKing@163.com>
2014-06-19 18:46:39 +0800

    tox商城 前台

--- Application/Shop/Controller/IndexController.class.php
+++ Application/Shop/Controller/IndexController.class.php
@@ -1,150 +1,139 @@
 <?php
 
 
-namespace Issue\Controller;
+namespace Shop\Controller;
 
 use Think\Controller;
 
 
 class IndexController extends Controller
 {
-    /**
-     * 业务逻辑都放在 WeiboApi 中
-     * @var
-     */
-    public function _initialize()
-    {
-        $tree = D('Issue')->getTree();
+    public function _goods_initialize(){
+        $tree = D('shopCategory')->getTree();
         $this->assign('tree', $tree);
+        $tox_money_cname=getToxMoneyName();
+        $this->assign('tox_money_cname',$tox_money_cname);
     }
 
-    public function index($page = 1, $issue_id = 0)
+    public function _myGoods_initialize(){
+        if(!is_login()){
+            $this->error('请先登录！');
+        }
+        $this->assign('type','my');
+        $tox_money_cname=getToxMoneyName();
+        $this->assign('tox_money_cname',$tox_money_cname);
+    }
+
+    public function index($page = 1, $category_id = 0)
     {
-        $issue_id=intval($issue_id);
-        $issue = D('Issue')->find($issue_id);
-        if (!$issue_id == 0) {
-            $issue_id = intval($issue_id);
-            $issues = D('Issue')->where("id=%d OR pid=%d",array($issue_id,$issue_id))->limit(999)->select();
+        $this->_goods_initialize();
+        $category_id=intval($category_id);
+        $goods_category = D('shopCategory')->find($category_id);
+        if ($category_id != 0) {
+            $category_id = intval($category_id);
+            $goods_categorys = D('shop_category')->where("id=%d OR pid=%d",array($category_id,$category_id))->limit(999)->select();
             $ids = array();
-            foreach ($issues as $v) {
+            foreach ($goods_categorys as $v) {
                 $ids[] = $v['id'];
             }
-            $map['issue_id'] = array('in', implode(',', $ids));
+            $map['category_id'] = array('in', implode(',', $ids));
         }
         $map['status'] = 1;
-        $content = D('IssueContent')->where($map)->order('create_time desc')->page($page, 16)->select();
-        $totalCount = D('IssueContent')->where($map)->count();
-        foreach ($content as &$v) {
-            $v['user'] = query_user(array('id', 'username', 'space_url', 'space_link', 'avatar128', 'rank_html'), $v['uid']);
-            $v['issue']=D('Issue')->field('id,title')->find($v['issue_id']);
+        $goods_list = D('shop')->where($map)->order('createtime desc')->page($page, 16)->select();
+        $totalCount = D('shop')->where($map)->count();
+        foreach ($goods_list as &$v) {
+            $v['category']=D('shopCategory')->field('id,title')->find($v['category_id']);
         }
         unset($v);
-        $this->assign('contents', $content);
+        $this->assign('contents', $goods_list);
         $this->assign('totalPageCount', $totalCount);
-        $this->assign('top_issue', $issue['pid'] == 0 ? $issue['id'] : $issue['pid']);
-
-        $this->assign('issue_id',$issue_id);
+        $this->assign('top_category', $goods_category['pid'] == 0 ? $goods_category['id'] : $goods_category['pid']);
+        $this->assign('category_id',$category_id);
         $this->display();
     }
 
-    public function doPost($id = 0, $cover_id = 0, $title = '', $content = '', $issue_id = 0)
+    public function goodsDetail($id = 0)
     {
-
-        if (!is_login()) {
-            $this->error('请登陆后再投稿。');
-        }
-        if (!$cover_id) {
-            $this->error('请上传封面。');
-        }
-        if (trim(op_t($title)) == '') {
-            $this->error('请输入标题。');
-        }
-        if (trim(op_h($content)) == '') {
-            $this->error('请输入内容。');
-        }
-        if ($issue_id == 0) {
-            $this->error('请选择分类。');
-        }
-        $content = D('IssueContent')->create();
-        $content['content']=op_h($content['content']);
-        $content['title']=op_t($content['title']);
-
-        if ($id) {
-            $content_temp = D('IssueContent')->find($id);
-            if (!is_administrator(is_login())) { //不是管理员则进行检测
-                if ($content_temp['uid'] != is_login()) {
-                    $this->error('小样儿，可别学坏。别以为改一下页面元素就能越权操作。');
-                }
-            }
-            $content['uid'] = $content_temp['uid']; //权限矫正，防止被改为管理员
-            $rs = D('IssueContent')->save($content);
-            if ($rs) {
-                $this->success('编辑成功。', U('issueContentDetail', array('id' => $content['id'])));
-            } else {
-                $this->success('编辑失败。', '');
-            }
-        } else {
-            if (C('NEED_VERIFY') && !is_administrator()) //需要审核且不是管理员
-            {
-                $content['status'] = 0;
-                $tip='但需管理员审核通过后才会显示在列表中，请耐心等待。';
-                $user=query_user(array('username'),is_login());
-                D('Common/Message')->sendMessage(C('USER_ADMINISTRATOR'),"{$user['username']}向专辑投了一份稿件，请到后台审核。" , $title = '专辑投稿提醒', U('Admin/Issue/verify'), is_login(), 2);
-            }
-            $rs = D('IssueContent')->add($content);
-            if ($rs) {
-                $this->success('投稿成功。'.$tip, 'refresh');
-            } else {
-                $this->success('投稿失败。', '');
-            }
-        }
-
-
-    }
-
-    public function issueContentDetail($id = 0)
-    {
-
-        $issue_content = D('IssueContent')->find($id);
-        if (!$issue_content) {
+        $this->_goods_initialize();
+        $goods = D('shop')->find($id);
+        if (!$goods) {
             $this->error('404 not found');
         }
-        D('IssueContent')->where(array('id' => $id))->setInc('view_count');
-        $issue = D('Issue')->find($issue_content['issue_id']);
+        $category = D('shopCategory')->find($goods['category_id']);
+        $this->assign('top_category', $category['pid'] == 0 ? $category['id'] : $category['pid']);
+        $this->assign('category_id', $category['id']);
+        $this->assign('category_title',$category['title']);
+        $this->assign('content', $goods);
+        $this->assign('my_tox_money',getMyToxMoney());
+        $this->display();
+    }
+    public function goodsBuy($id=0,$num=1){
+        if(!is_login()){
+            $this->error('请先登录！');
+        }
+        $goods=D('shop')->where('id='.$id)->find();
+        if($goods){
+            //判断商品余量
+            if($num>$goods['goods_num']){
+                $this->error('商品余量不足');
+            }
+            $data['goods_id']=$id;
+            $data['goods_num']=$num;
+            $data['status']=-1;
+            $data['uid']=is_login();
+            $data['createtime']=time();
 
-        $this->assign('top_issue', $issue['pid'] == 0 ? $issue['id'] : $issue['pid']);
-        $this->assign('issue_id', $issue['id']);
-        $issue_content['user'] = query_user(array('id', 'username', 'space_url', 'space_link', 'avatar64', 'rank_html', 'signature'), $issue_content['uid']);
-        $this->assign('content', $issue_content);
+            //扣tox_money
+            $tox_money_need=$num*$goods['tox_money_need'];
+            $my_tox_money=getMyToxMoney();
+            if($tox_money_need>$my_tox_money){
+                $this->error('你的'.getToxMoneyName().'不足');
+            }
+            D('member')->where('uid='.is_login())->setDec('tox_money',$tox_money_need);
+            $res=D('shop_buy')->add($data);
+            if($res){
+                D('shop')->where('id='.$id)->setDec('goods_num',$num);
+                $this->success('购买成功！花费了'.$tox_money_need.getToxMoneyName(),U('Index/myGoods',array('status'=>-1)));
+            }else{
+                $this->error('购买失败！');
+            }
+        }else{
+            $this->error('请选择要购买的商品');
+        }
+    }
+
+    public function myGoods($page = 1,$status=-1){
+        $this->_myGoods_initialize();
+        $map['status'] = $status;
+        $map['uid']=is_login();
+        $goods_buy_list=D('shop_buy')->where($map)->page($page,16)->order('createtime desc')->select();
+        $totalCount =D('shop_buy')->where($map)->count();
+        foreach ($goods_buy_list as &$v) {
+            $v['goods']=D('shop')->where('id='.$v['goods_id'])->find();
+            $v['category']=D('shopCategory')->field('id,title')->find($v['goods']['category_id']);
+        }
+        unset($v);
+        $this->assign('contents', $goods_buy_list);
+        $this->assign('totalPageCount', $totalCount);
+        $this->assign('status',$status);
         $this->display();
     }
 
-    public function selectDropdown($pid)
-    {
-        $issues = D('Issue')->where(array('pid' => $pid, 'status' => 1))->limit(999)->select();
-        exit(json_encode($issues));
-
-
-    }
-
-    public function edit($id)
-    {
-        $issue_content = D('IssueContent')->find($id);
-        if (!$issue_content) {
+    public function applyGoods($id=0){
+        if(!is_login()){
+            $this->error('请先登录！');
+        }
+        $goods_buy = D('shop_buy')->find($id);
+        if (!$goods_buy) {
             $this->error('404 not found');
         }
-        if (!is_administrator(is_login())) { //不是管理员则进行检测
-            if ($issue_content['uid'] != is_login()) {
-                $this->error('404 not found');
-            }
+        $data['applytime']=time();
+        $data['status']=0;
+        $res=D('shop_buy')->where('id='.$id)->save($data);
+        if($res){
+            $this->success('申领成功，请等待管理员发货',U('myGoods'));
+        }else{
+            $this->error('申领失败！');
         }
-
-        $issue = D('Issue')->find($issue_content['issue_id']);
-
-        $this->assign('top_issue', $issue['pid'] == 0 ? $issue['id'] : $issue['pid']);
-        $this->assign('issue_id', $issue['id']);
-        $issue_content['user'] = query_user(array('id', 'username', 'space_url', 'space_link', 'avatar64', 'rank_html', 'signature'), $issue_content['uid']);
-        $this->assign('content', $issue_content);
-        $this->display();
     }
 }
