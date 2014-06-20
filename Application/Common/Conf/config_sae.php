80d4697e992f48faecfa340b6ded09b0033a68f5
陈一枭 <yhtt2020@foxmail.com>
2014-06-20 15:43:07 +0800

    兼容图片的上传

--- Application/Common/Conf/config_sae.php
+++ Application/Common/Conf/config_sae.php
@@ -7,11 +7,17 @@
  * 版权所有 想天软件工作室(www.ourstu.com)
  */
 return array(
-    'DB_HOST'			=> 	SAE_MYSQL_HOST_M.','.SAE_MYSQL_HOST_S, // 服务器地址
-    'DB_NAME'			=> 	SAE_MYSQL_DB,        // 数据库名
-    'DB_USER'			=> 	SAE_MYSQL_USER,    // 用户名
-    'DB_PWD'			=> 	SAE_MYSQL_PASS,         // 密码
-    'DB_PORT'			=> 	SAE_MYSQL_PORT,        // 端口
+    'DB_HOST' => SAE_MYSQL_HOST_M . ',' . SAE_MYSQL_HOST_S, // 服务器地址
+    'DB_NAME' => SAE_MYSQL_DB, // 数据库名
+    'DB_USER' => SAE_MYSQL_USER, // 用户名
+    'DB_PWD' => SAE_MYSQL_PASS, // 密码
+    'DB_PORT' => SAE_MYSQL_PORT, // 端口
+
+
+    'PICTURE_UPLOAD_DRIVER' => 'Sae',
+    'UPLOAD_SAE_CONFIG' => array(
+        'rootPath' => '.http://' . $_SERVER['HTTP_APPNAME'] . '-uploads.stor.sinaapp.com/Picture',
+        'domain'=>'uploads')
 );
 
 //本文件兼容sae访问
