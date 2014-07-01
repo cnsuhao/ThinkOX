2688180438a9891f066159f2cd47cf151eea6c5f
郑钟良 <WebServiceSoftKing@163.com>
2014-07-01 11:06:57 +0800

    个人中心，粉丝、关注

--- Application/Usercenter/Controller/IndexController.class.php
+++ Application/Usercenter/Controller/IndexController.class.php
@@ -16,12 +16,17 @@ class IndexController extends BaseController{
 
     public function _initialize(){
         parent::_initialize();
+        $uid=isset($_GET['uid'])?op_t($_GET['uid']):is_login();
+        //调用API获取基本信息
+        $this->userInfo($uid);
+
+        $this->_fans_and_following($uid);
+
     }
 
     public function index($uid = null,$page=1,$count=10)
     {
-        //调用API获取基本信息
-        $this->userInfo($uid);
+
 
         $appArr=$this->_tab_menu();
         foreach($appArr as $key=>$val){
@@ -53,8 +58,6 @@ class IndexController extends BaseController{
 
 
     public function appList ($uid=null,$page=1,$count=10) {
-        //调用API获取基本信息
-        $this->userInfo($uid);
 
         $appArr=$this->_tab_menu();
 
@@ -110,15 +113,43 @@ class IndexController extends BaseController{
         return $appArr;
     }
 
+
+    public function _fans_and_following($uid=null){
+        $uid=isset($uid)?$uid:is_login();
+        //我的粉丝展示
+        $map['follow_who'] = $uid;
+        $fans_default = D('Follow')->where($map)->field('who_follow')->order('create_time desc')->limit(8)->select();
+        $fans_totalCount = D('Follow')->where($map)->count();
+        foreach ($fans_default as &$user) {
+            $user['user'] = query_user(array('avatar64', 'uid', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'), $user['who_follow']);
+        }
+        unset($user);
+        $this->assign('fans_totalCount',$fans_totalCount);
+        $this->assign('fans_default',$fans_default);
+
+        //我关注的展示
+        $map_follow['who_follow'] = $uid;
+        $follow_default = D('Follow')->where($map_follow)->field('follow_who')->order('create_time desc')->limit(8)->select();
+        $follow_totalCount = D('Follow')->where($map_follow)->count();
+        foreach ($follow_default as &$user) {
+            $user['user'] = query_user(array('avatar64', 'uid', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'), $user['follow_who']);
+        }
+        unset($user);
+        $this->assign('follow_totalCount',$follow_totalCount);
+        $this->assign('follow_default',$follow_default);
+        dump($fans_default);
+    }
+
     public function fans($uid=null,$page = 1)
     {
+        $uid=isset($uid)?$uid:is_login();
         //调用API获取基本信息
         $this->userInfo($uid);
         $this->_tab_menu();
 
 
         $this->assign('tab', 'fans');
-        $fans = D('Follow')->getFans(is_login(), $page, array('avatar128', 'id', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'),$totalCount);
+        $fans = D('Follow')->getFans($uid, $page, array('avatar128', 'uid', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'),$totalCount);
         $this->assign('fans', $fans);
         $this->assign('totalCount',$totalCount);
         $this->display();
@@ -126,11 +157,12 @@ class IndexController extends BaseController{
 
     public function following($uid=null,$page=1)
     {
+        $uid=isset($uid)?$uid:is_login();
         //调用API获取基本信息
         $this->userInfo($uid);
         $this->_tab_menu();
 
-        $following = D('Follow')->getFollowing(is_login(), $page, array('avatar128', 'id', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'),$totalCount);
+        $following = D('Follow')->getFollowing($uid, $page, array('avatar128', 'uid', 'nickname', 'fans', 'following', 'weibocount', 'space_url','title'),$totalCount);
         $this->assign('following',$following);
         $this->assign('totalCount',$totalCount);
         $this->assign('tab', 'following');
