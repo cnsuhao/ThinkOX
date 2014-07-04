3b4a2099427816698e3d7fd4269602c2edd05db7
郑钟良 <WebServiceSoftKing@163.com>
2014-07-04 11:56:40 +0800

    【修复】小名片

--- Public/Core/js/core.js
+++ Public/Core/js/core.js
@@ -203,15 +203,15 @@ function ucard() {
                         '<div class="col-xs-3">'+
                             '<img src="{$userProfile.avatar64}" class="avatar-img img-responsive" style="-webkit-box-shadow: 0 3px 4px rgba(11, 2, 5, 0.54);-moz-box-shadow: 0 3px 4px rgba(11, 2, 5, 0.54);box-shadow: 0 3px 4px rgba(173, 173, 173, 0.54);border: solid 2px #fff;"/>'+
                         '</div>'+
-                        '<div class="col-xs-9" style="padding-top: 25px;">'+
-                        '<div style="font-size: 16px;font-weight: bold;"><a href="{$userProfile.space_url}" title="">{$userProfile.nickname}</a>'+
+                        '<div class="col-xs-9" style="padding-top: 25px;padding-right:0px;font-size: 12px;">'+
+                        '<div style="font-size: 16px;font-weight: bold;"><a href="{$userProfile.space_url}" title="">{$userProfile.nickname}</a>{$userProfile.rank_link}'+
                         '</div>'+
                         '<div>'+
                         '<a href="{$userProfile.following_url}" title="我的关注" target="_black">关注：{$userProfile.following}</a>&nbsp;&nbsp;&nbsp;&nbsp;'+
                         '<a href="{$userProfile.fans_url}" title="我的关注" target="_black">粉丝：{$userProfile.fans}</a>&nbsp;&nbsp;&nbsp;&nbsp;'+
                         '<a href="{$userProfile.weibo_url}" title="我的关注" target="_black">微博：{$userProfile.weibocount}</a>'+
                     '</div>'+
-                        '<div style="margin-bottom: 15px">'+
+                        '<div style="margin-bottom: 15px;color: #848484">'+
                         '个性签名：'+
                             '<span>'+
                                 '{$userProfile.signature}'+
@@ -223,6 +223,7 @@ function ucard() {
                         follow+
                         '</div>'+
                     '</div>';
+
                     userProfile.signature = userProfile.signature === '' ? '还没想好O(∩_∩)O' : userProfile.signature;
                     for(var key in userProfile){
                         html=html.replace('{$userProfile.'+key+'}',userProfile[key]);
