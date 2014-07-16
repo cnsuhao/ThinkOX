9fce3d9fafe3432b57a16c3b102ce6f65f334177
陈一枭 <yhtt2020@foxmail.com>
2014-07-16 10:26:24 +0800

    完善消息模型的注释

--- Application/Common/Model/MessageModel.class.php
+++ Application/Common/Model/MessageModel.class.php
@@ -74,13 +74,21 @@ class MessageModel extends Model
     }
 
 
+
     /**
-     * @param $to_uid 接受消息的用户ID
+     * 注：appname及之后的参数，一般情况下无需填写
+     * @param        $to_uid 接受消息的用户ID
      * @param string $content 内容
      * @param string $title 标题，默认为  您有新的消息
-     * @param $url 链接地址，不提供则默认进入消息中心
-     * @param $int $from_uid 发起消息的用户，根据用户自动确定左侧图标，如果为用户，则左侧显示头像
-     * @param int $type 消息类型，0系统，1用户，2应用
+     * @param        $url 链接地址，不提供则默认进入消息中心
+     * @param int    $from_uid 发起消息的用户，根据用户自动确定左侧图标，如果为用户，则左侧显示头像
+     * @param int    $type 消息类型，0系统，1用户，2应用
+     * @param string $appname 应用名，默认不需填写，如果填写了就必须实现对应的消息处理模型，例如贴吧里面可以基于某个回复开启会话
+     * @param string $apptype 同上，应用里面的一个标识符
+     * @param int    $source_id 来源ID，通过来源ID获取基于XX会话的来源信息
+     * @param int    $find_id 查找ID，通过查找ID获得标识ID
+     * @return int
+     * @auth 陈一枭
      */
     public function sendMessage($to_uid, $content = '', $title = '您有新的消息', $url, $from_uid = 0, $type = 0, $appname = '', $apptype = '', $source_id = 0, $find_id = 0)
     {
