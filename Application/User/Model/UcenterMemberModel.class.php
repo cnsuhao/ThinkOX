2e9511c256ffef54e772069867150011cba50e23
陈一枭 <yhtt2020@foxmail.com>
2014-07-18 14:25:20 +0800

    调整。

--- Application/User/Model/UcenterMemberModel.class.php
+++ Application/User/Model/UcenterMemberModel.class.php
@@ -399,6 +399,7 @@ class UcenterMemberModel extends Model
 
         $data['username'] = $this->rand_username();
         $data['email'] = $this->rand_email();
+        $data['password']=$this->create_rand();
         $data1 = $this->create($data);
         $uid = $this->add($data1);
         return $uid;
