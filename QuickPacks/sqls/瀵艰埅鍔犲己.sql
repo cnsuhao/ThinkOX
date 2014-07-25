ffcfb302240135cea6ac6fda61eca5506a53106e
陈一枭 <yhtt2020@foxmail.com>
2014-07-25 10:45:35 +0800

    数据库补充

---
+++ QuickPacks/sqls/瀵艰埅鍔犲己.sql
@@ -0,0 +1,3 @@
+ALTER TABLE  `thinkox_channel` ADD  `color` VARCHAR( 30) NOT NULL;
+ALTER TABLE  `thinkox_channel` ADD  `band_color` VARCHAR( 30) NOT NULL;
+ALTER TABLE  `thinkox_channel` ADD  `band_text` VARCHAR( 30) NOT NULL;
