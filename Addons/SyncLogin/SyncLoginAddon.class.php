01621e043f6f0eff9160af17fd552f221a5185a3
陈一枭 <yhtt2020@foxmail.com>
2014-07-29 10:03:07 +0800

    修正同步登陆无法多个社交账号登陆同一个站内账号的bug

--- Addons/SyncLogin/SyncLoginAddon.class.php
+++ Addons/SyncLogin/SyncLoginAddon.class.php
@@ -26,12 +26,26 @@ class SyncLoginAddon extends Addon
         $prefix = C("DB_PREFIX");
         $model = D();
         $model->execute("DROP TABLE IF EXISTS {$prefix}sync_login;");
-        $model->execute("CREATE TABLE {$prefix}sync_login ( `uid` int(11) NOT NULL,  `type_uid` varchar(255) NOT NULL,  `type` varchar(255) NOT NULL,  `oauth_token` varchar(255) NOT NULL,  `oauth_token_secret` varchar(255) NOT NULL,  `is_sync` tinyint(4) NOT NULL,  PRIMARY KEY (`uid`))");
+        $model->execute("
+        CREATE TABLE IF NOT EXISTS `{$prefix}sync_login` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `type_uid` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `oauth_token` varchar(255) NOT NULL,
+  `oauth_token_secret` varchar(255) NOT NULL,
+  `is_sync` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+");
+
         return true;
     }
 
     public function uninstall()
     {
+        $prefix = C("DB_PREFIX");
+        D('')->execute("DROP TABLE IF EXISTS {$prefix}sync_login;");
         return true;
     }
 
---
+++ QuickPacks/sqls/淇鍚屾鐧婚檰.sql
@@ -0,0 +1,4 @@
+Alter table thinkox_sync_login drop primary key
+ALTER TABLE  `thinkox_sync_login` ADD  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST
+
+
