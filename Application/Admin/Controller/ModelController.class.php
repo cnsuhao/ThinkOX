4dfcc16299c0c7f91c335af45f355746bfafe8c8
陈一枭 <yhtt2020@foxmail.com>
2014-08-04 10:50:08 +0800

    【修正】分类和模型控制器左侧菜单不统一的问题

--- Application/Admin/Controller/ModelController.class.php
+++ Application/Admin/Controller/ModelController.class.php
@@ -16,6 +16,88 @@ use Admin\Model\AuthGroupModel;
  */
 class ModelController extends AdminController {
 
+    public function _initialize(){
+        parent::_initialize();
+        $this->getMenu();
+    }
+    /**
+     * 显示左边菜单，进行权限控制
+     * @author huajie <banhuajie@163.com>
+     */
+    protected function getMenu()
+    {
+        //获取动态分类
+        $cate_auth = AuthGroupModel::getAuthCategories(UID); //获取当前用户所有的内容权限节点
+        // dump($cate_auth);exit;
+        $cate_auth = $cate_auth == null ? array() : $cate_auth;
+        $cate = M('Category')->where(array('status' => 1))->field('id,title,pid,allow_publish')->order('pid,sort')->select();
+
+        //没有权限的分类则不显示
+        if (!IS_ROOT) {
+            foreach ($cate as $key => $value) {
+                if (!in_array($value['id'], $cate_auth)) {
+                    unset($cate[$key]);
+                }
+            }
+        }
+
+        $cate = list_to_tree($cate); //生成分类树
+
+        //获取分类id
+        $cate_id = I('param.cate_id');
+        $this->cate_id = $cate_id;
+
+        //是否展开分类
+        $hide_cate = true;
+
+        //生成每个分类的url
+        foreach ($cate as $key => &$value) {
+            $value['url'] = 'Article/index?cate_id=' . $value['id'];
+            if ($cate_id == $value['id'] && $hide_cate) {
+                $value['current'] = true;
+            } else {
+                $value['current'] = false;
+            }
+            if (!empty($value['_child'])) {
+                $is_child = false;
+                foreach ($value['_child'] as $ka => &$va) {
+                    $va['url'] = 'Article/index?cate_id=' . $va['id'];
+                    if (!empty($va['_child'])) {
+                        foreach ($va['_child'] as $k => &$v) {
+                            $v['url'] = 'Article/index?cate_id=' . $v['id'];
+                            $v['pid'] = $va['id'];
+                            $is_child = $v['id'] == $cate_id ? true : false;
+                        }
+                    }
+                    //展开子分类的父分类
+                    if ($va['id'] == $cate_id || $is_child) {
+                        $is_child = false;
+                        if ($hide_cate) {
+                            $value['current'] = true;
+                            $va['current'] = true;
+                        } else {
+                            $value['current'] = false;
+                            $va['current'] = false;
+                        }
+                    } else {
+                        $va['current'] = false;
+                    }
+                }
+            }
+        }
+        $this->assign('nodes', $cate);
+
+        $this->assign('cate_id', $this->cate_id);
+
+        //获取面包屑信息
+        $nav = get_parent_category($cate_id);
+        $this->assign('rightNav', $nav);
+        //获取回收站权限
+        $show_recycle = $this->checkRule('Admin/article/recycle');
+        $this->assign('show_recycle', IS_ROOT || $show_recycle);
+        //获取草稿箱权限
+        $this->assign('show_draftbox', C('OPEN_DRAFTBOX'));
+    }
 
     /**
      * 检测是否是需要动态判断的权限
@@ -64,6 +146,7 @@ class ModelController extends AdminController {
      * @author huajie <banhuajie@163.com>
      */
     public function index(){
+
         $map = array('status'=>array('gt',-1));
         $list = $this->lists('Model',$map);
         int_to_string($list);
@@ -72,6 +155,7 @@ class ModelController extends AdminController {
 
         $this->assign('_list', $list);
         $this->meta_title = '模型管理';
+        $this->getMenu();
         $this->display();
     }
 
